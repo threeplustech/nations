da752786e5d56eb4fc1df7340bbb1f3e7e3febcc
bharatnadkarni <bha.nadkarni@gmail.com>
2020-08-26 16:12:23 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser');
+const path = require('path');
+const nations = require("./nations");
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+nations.setJson(path.join(__dirname,"/nations.json"));
+console.log(path.join(__dirname,"/nations.json"));
+
+app.get("/getCountries", (req, res, next)=>{
+  var countries = nations.getCountries();
+  if(countries){
+    res.json({status: 200, data: {countries: countries}, message: "Countries fetched successfully"});
+  } else {
+    res.json({status: 240, data: null, message: "Server error. Please contact admin."});
+  }
+  
+})
+
+app.get("/getStatesForCountry/:country", (req, res, next)=>{
+  var states = nations.getStatesForCountry(req.params.country);
+  if(states){
+    res.json({status: 200, data: {states: states}, message: "States fetched successfully"});
+  } else {
+    res.json({status: 400, data: null, message: "Could not get states for this country"});
+  }
+})
+
+app.get("getCitiesForState/:country/:state", (req, res, next)=>{
+  var cities = nations.getCityForState(req.params.country, req.params.state);
+  if(cities){
+    res.json({status: 200, data: {cities: cities}, message: "Cities fetched successfully"});
+  } else {
+    res.json({status: 400, data: null, message: "Could not get cities for this state"});
+  }
+})
+
+
+app.listen(port, () => {
+  console.log(`Nations app listening at http://localhost:3000`)
+})
+
+
